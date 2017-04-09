b96478ff938886edac5f8427cdf8d93408af43af
Evaldas <evaldaskal@gmail.com>
2017-04-09 16:44:22 +0300

    added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,11 +8,24 @@ var {User} = require('./../models/user');
 //testing lifecycle method;
 //lets run some code before any tests
 //only gona move to test cases once done is called
+// beforeEach((done)=>{
+//     Todo.remove({}).then(()=>{
+//         //wipe all todos
+//         done();
+//     });
+// });
+
+//Creating dummy todos
+const todos = [{
+    text: "First test todo"
+}, {
+    text: "Second test todo"
+}];
+
 beforeEach((done)=>{
     Todo.remove({}).then(()=>{
-        //wipe all todos
-        done();
-    });
+        return Todo.insertMany(todos); //return allows to chain callbacks
+    }).then(()=> done());
 });
 
 describe('POST /todos',()=>{
@@ -34,7 +47,7 @@ describe('POST /todos',()=>{
                 return done(err);
             }
 
-            Todo.find().then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 //if either of these fails test still gona pass thus catch is required
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
@@ -54,9 +67,23 @@ describe('POST /todos',()=>{
             }
 
             Todo.find().then((todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((err)=> done(err));
         })
-    });
-});
+    }); //end not create with wrong data
+
+}); //end describe POST
+
+describe('GET /todos',()=>{
+      it('shoud get all todos', (done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+      }); //end get all todos test
+
+});//end describe GET
