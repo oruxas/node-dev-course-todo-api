3b1edc7faf2963c9212340fde1f6f6fb295ed869
Evaldas <evaldaskal@gmail.com>
2017-04-13 22:08:55 +0300

    added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
 
-var {ObjectId} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -74,6 +76,30 @@ app.delete('/todos/:id', (req, res)=>{
     });
 });
 
+app.patch('/todos/:id',(req, res)=>{
+    var id = req.params.id;
+    //so  that user wouldn't be able to update whatever 
+    var body = _.pick(req.body, ['text', 'completed']);   //pass req.body and takes array of properties if exist
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();    //num of secs since 1997 jan 1, greater than 0 values forward, less than 0 in the past
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((err)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
